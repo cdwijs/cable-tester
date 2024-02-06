7ee5035fdbb023aba3e521b3c644a59e859d0e1c
Cedric de Wijs <c.r.l.de_wijs@lumc.nl>
2024-02-06 09:14:05 +0100

    initial state machine, initial doxygen

--- cable-tester/sampler.c
+++ cable-tester/sampler.c
@@ -1,8 +1,19 @@
+#include "sampler.h"
 #include <pico/stdlib.h> //for gpio_put and sleep_ms
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
+TSAMPLER mySamplerData;
+const TSAMPLER *mySampler = &mySamplerData;
+
 void samplerExecute(void)
 {
+    switch(mySampler->state)
+    {
+        case ST_SAMP_WAIT:
+        {
+            break;
+        }
+    }
     gpio_put(LED_PIN, 1);
     sleep_ms(100);
 }
--- cable-tester/sampler.h
+++ cable-tester/sampler.h
@@ -1,6 +1,44 @@
 #ifndef SAMPLER_H
 #define SAMPLER_H
 
+/** \page main
+    Sampler \n
+    \dot
+digraph
+{
+    node [shape=record, color=red];
+    edge [arrowhead=open,color=green];
+
+    rankdir="TD"
+
+    wait -> start [ label = "trigger" ];
+    start -> scan [ label = "" ];
+    scan -> wait  [ label = "done" ];
+}
+    \enddot
+*/
+
+#include <stdint.h> //for uint8_t
+
+typedef enum
+{
+    ST_SAMP_WAIT = 0,
+
+}TSAMPLERSTATE;
+
+
+typedef struct
+{
+    uint8_t trigger: 1;
+}TSAMPLERFLAGS;
+
+typedef struct
+{
+    TSAMPLERSTATE           state;
+    TSAMPLERFLAGS           flags;
+    uint8_t                 index;
+}TSAMPLER;
+
 void samplerExecute(void);
 
 #endif //SAMPLER_H
